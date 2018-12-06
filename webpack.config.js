c561e124f173fa06143fa186f05e446a3c2b9603
Bryce Jech <bryce@brycejech.com>
2018-12-05 21:13:32 -0600

    target all module syntaxes

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,13 @@ const path = require('path');
 module.exports = {
     entry: './src/text-contrast.js',
     output: {
-        path: path.resolve('./dist'),
-        filename: 'text-contrast.min.js'
+        path:      path.resolve('./dist'),
+        filename: 'text-contrast.min.js',
+        // Will wrap in 'textContrast' namespace
+        // Omit since we're returning a namespaced object already
+        // library:  'textContrast',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
     module: {
 		rules: [
